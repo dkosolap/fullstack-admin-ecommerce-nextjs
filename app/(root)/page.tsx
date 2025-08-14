4ee1badb7acf8456c2d38ad683e77118d8218530
Dmytro Kosolap <dmytro.kosolap@uptech.team>
2025-08-14 16:02:24 -0700

    init

---
+++ app/(root)/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { useEffect } from "react";
+
+import { useStoreModal } from "@/hooks/use-store-modal";
+
+export default function SetupPage() {
+  const { isOpen, onOpen } = useStoreModal();
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
+  return (
+    <div className="p-4">
+      Root Page
+    </div>
+  );
+}
